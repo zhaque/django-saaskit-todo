4c613cc2951bf9067a58f0d57fd4f294d573ad6b
Stepan Rakhimov <strakh@macbook.local>
2009-10-15 12:46:19 +0400

    scratchpad quotas

--- todo/urls.py
+++ todo/urls.py
@@ -9,6 +9,7 @@ urlpatterns = patterns('',
     url(r'^(?P<list_id>\d{1,4})/(?P<list_slug>[\w-]+)$', 'todo.views.view_list', name='todo-incomplete_tasks'),
     url(r'^(?P<list_id>\d{1,4})/(?P<list_slug>[\w-]+)/completed$', 'todo.views.view_list', {'view_completed':1},name='todo-completed_tasks'),    
     url(r'^$', 'todo.views.list_lists',name="todo-lists"),
+    url(r'^not_available/$', direct_to_template, dict(template='todo/no_available.html'), name="todo-not-available"),
     
     # View reorder_tasks is only called by JQuery for drag/drop task ordering
     url(r'^reorder_tasks/$', 'todo.views.reorder_tasks',name="todo-reorder_tasks"),
